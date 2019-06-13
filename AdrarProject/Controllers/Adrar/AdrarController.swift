bff603016d95b97bffaf5362caa5a13e5f846c96
vincent <vincentmald@gmail.com>
2019-06-13 11:14:24 +0200

    Ajout de 2 contrôler et StoryB

---
+++ AdrarProject/Controllers/Adrar/AdrarController.swift
@@ -0,0 +1,25 @@
+//
+//  AdrarController.swift
+//  AdrarProject
+//
+//  Created by Vincent Maldonado on 04/06/2019.
+//  Copyright © 2019 Vincent Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class AdrarController: UIViewController {
+    
+    @IBOutlet weak var AdrarPresentation: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //Revenir en arriere
+@IBAction func backMenuFromAdrar(_ sender: Any) {
+       dismiss(animated: true, completion: nil)
+        
+    }
+}
---
+++ AdrarProject/Views/SegueFromLeft.swift
@@ -0,0 +1,30 @@
+//
+//  SegueFromLeft.swift
+//  AdrarProject
+//
+//  Created by Vincent Maldonado on 12/06/2019.
+//  Copyright © 2019 Vincent Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class SegueFromLeft:UIStoryboardSegue {
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        },
+                       completion: { finished in
+                        src.present(dst, animated: false, completion: nil)
+        }
+        )
+    }
+}
